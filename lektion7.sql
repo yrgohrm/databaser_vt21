44e967390969799d9fdad1ab6a79719c6695adf9
Hampus Ram <hampus.ram@educ.goteborg.se>
2021-04-29 16:28:34 +0200

    liten sats för att lista folk i roller

--- lektion7.sql
+++ lektion7.sql
@@ -74,3 +74,17 @@ REVOKE EXECUTE ON SCHEMA :: test FROM test_role;
 GRANT EXECUTE ON OBJECT :: test.doSomeStuff TO test_role;
 
 DENY INSERT ON OBJECT :: test.small_numbers TO test_role;
+
+
+-- Rasmus härliga användande av sys-vyerna för att lista användare i roller
+-- utan att titta med ssms
+
+SELECT DP1.name AS DatabaseRoleName,   
+   isnull (DP2.name, 'No members') AS DatabaseUserName   
+ FROM sys.database_role_members AS DRM  
+ RIGHT OUTER JOIN sys.database_principals AS DP1  
+   ON DRM.role_principal_id = DP1.principal_id  
+ LEFT OUTER JOIN sys.database_principals AS DP2  
+   ON DRM.member_principal_id = DP2.principal_id  
+WHERE DP1.type = 'R'
+ORDER BY DP1.name; 
